754f3d935101bfabcead91fdaa61900ca22bbbb3
jee45 <jee45@txstate.edu>
2016-08-21 13:17:22 -0500

    Update models.py
    made favorites table

--- models.py
+++ models.py
@@ -16,5 +16,22 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    
+    username = db.Column(db.String(60))
+    property_id = db.Column(db.String(60))
+    comment = db.Text()
+    
+    def __init__(self,username, property_id, comment):
+        self.username = username
+        self.property_id = property_id
+        self.comment = comment
+    
+    
+    
+    
 #create
 db.create_all(app=app)
