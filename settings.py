8ba24ae75c5d7ec13c6c22c4dde19e2cd7741eea
Sam Bass <sambass@txstate.edu>
2016-08-21 12:41:08 -0500

    changed to my SQLALCHEMY_DATABASE_URI in heroku

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ SECRET_KEY =  'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 ADMIN_PASSWORD = 'admin'
 
-SQLALCHEMY_DATABASE_URI  = 'postgres://vgbbklbhhxbnrt:zQZiRRek9ZwPj7A67EwgdKbku_@ec2-23-21-179-195.compute-1.amazonaws.com:5432/d4813228v51lhq'
+SQLALCHEMY_DATABASE_URI  = 'postgres://sylyekhubkraec:_DDyvhNFG-PM2N8vW_v963GHu1@ec2-54-235-240-76.compute-1.amazonaws.com:5432/dft1scan7sme15'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
